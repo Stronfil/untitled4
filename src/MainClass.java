fa4a4451c159a35c4ab2f83337635f902879aa9c
Stronfil <tambovskii99@gmail.com>
2021-01-20 14:10:34 +0300

    Initial commit

---
+++ src/MainClass.java
@@ -0,0 +1,21 @@
+public class MainClass {
+    private static final int SIZE = 10000000;
+    private static final int HALF_SIZE = SIZE / 2;
+
+    public float[] calculate(float[] arr) {
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + arr[i] / 5) * Math.cos(0.2f + arr[i] / 5) * Math.cos(0.4f + arr[i] / 2));
+        return arr;
+    }
+
+    public void runOneThread() {
+        float[] arr = new float[SIZE];
+        for (int i = 0; i < arr.length; i++) arr[i] = 1.0f;
+        long a = System.currentTimeMillis();
+        calculate(arr);
+        System.out.println("One thread method ends with: " + (System.currentTimeMillis() - a));
+    }
+
+   
+}
+
